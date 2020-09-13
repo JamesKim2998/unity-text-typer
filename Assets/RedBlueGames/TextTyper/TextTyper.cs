75cb7697ab5a50594aaf0d1418c49b28cbf188e5
김대원 <james.kim@devsisters.com>
2020-09-13 21:38:02 +0900

    타이핑속도 더 빠르게.

--- Assets/RedBlueGames/TextTyper/TextTyper.cs
+++ Assets/RedBlueGames/TextTyper/TextTyper.cs
@@ -15,7 +15,7 @@
         /// <summary>
         /// The print delay setting. Could make this an option some day, for fast readers.
         /// </summary>
-        private const float PrintDelaySetting = 0.018f;
+        private const float PrintDelaySetting = 0.01f;
 
         // Characters that are considered punctuation in this language. TextTyper pauses on these characters
         // a bit longer by default. Could be a setting sometime since this doesn't localize.
